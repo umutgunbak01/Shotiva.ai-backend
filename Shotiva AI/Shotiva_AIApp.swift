44130f3440449b8339839c741d5d0f0a6bb7d72e
umutgunbak01 <umutgunbak@gmail.com>
2025-05-11 00:17:55 +0300

    Initial Commit

---
+++ Shotiva AI/Shotiva_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shotiva_AIApp.swift
+//  Shotiva AI
+//
+//  Created by Umut Gunbak on 11.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct Shotiva_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
